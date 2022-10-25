4d3f0393fe09d82a76c7c9a2ccd2d9867b9b1f75
Ayush Saxena <ayushsaxenarockz1906@gmail.com>
2022-10-25 23:40:03 +0530

    Implemented Base Entity (JPA). Closes #35.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,8 +1,18 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
-
+/*
+    Mapped superclass annotation establishes it as a
+    base class to JPA
+ */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
