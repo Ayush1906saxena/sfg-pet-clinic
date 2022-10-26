a5ef1eb69036edab320f4e3f0e31bb3b5bf93234
Ayush Saxena <ayushsaxenarockz1906@gmail.com>
2022-10-26 09:28:34 +0530

    Created Spring Data JPA Services for Owners.Closes #40.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -4,5 +4,5 @@ import guru.springframework.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
-
+    Owner findByLastName(String lastName);
 }
