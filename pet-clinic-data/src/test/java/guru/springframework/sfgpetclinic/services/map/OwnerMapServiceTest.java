251cba31ef294904cef372228d67917bb951398a
Ayush Saxena <ayushsaxenarockz1906@gmail.com>
2022-10-31 22:46:30 +0530

    Made a change to OwnerMapServiceTest.

---
+++ pet-clinic-data/src/test/java/guru/springframework/sfgpetclinic/services/map/OwnerMapServiceTest.java
@@ -0,0 +1,40 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class OwnerMapServiceTest {
+    OwnerMapService ownerMapService;
+
+    @BeforeEach
+    void setUp() {
+        ownerMapService = new OwnerMapService(new PetTypeMapService(), new PetMapService());
+        ownerMapService.save(Owner.builder().id(5L).build());
+    }
+
+    @Test
+    void findAll() {
+
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByLastName() {
+    }
+}
